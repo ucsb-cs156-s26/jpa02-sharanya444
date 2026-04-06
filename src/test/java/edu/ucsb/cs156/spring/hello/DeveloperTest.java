117bbe71648c099b07775389452ee704f906b435
Sharanya Gehlot <sgehlot@ucsb.edu>
2026-04-06 16:30:33 -0700

    updated with developer name

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -24,7 +24,7 @@ public class DeveloperTest {
     public void getName_returns_correct_name() {
         // TODO: Replace Chris G. with your name as shown on
         // <https://bit.ly/cs156-s26-teams>
-        assertEquals("Chris G.", Developer.getName());
+        assertEquals("Sharanya", Developer.getName());
     }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
